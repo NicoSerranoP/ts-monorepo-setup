1ead194281d0bc187a80087217f8326796a5630a
Nico Serrano <nnicosp@hotmail.com>
2026-07-09 02:13:28 -0500

    chore(eslint): use prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import unusedImports from "eslint-plugin-unused-imports";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 import { readFileSync } from "fs";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
-const prettierConfig = readFileSync(new URL("./.prettierrc.json", import.meta.url), "utf8");
+const currentFilePath = fileURLToPath(import.meta.url);
+const prettierConfig = readFileSync(resolve(currentFilePath, "../.prettierrc.json"), "utf8");
 const prettierOptions = JSON.parse(prettierConfig);
 
 export const baseConfig = tseslint.config(
@@ -16,6 +20,7 @@ export const baseConfig = tseslint.config(
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: {
+      prettier,
       "unused-imports": unusedImports,
     },
     languageOptions: {
@@ -28,7 +33,6 @@ export const baseConfig = tseslint.config(
       "@typescript-eslint/no-floating-promises": "warn",
       "@typescript-eslint/no-unsafe-argument": "warn",
       "no-console": "error",
-      "unused-imports/no-unused-imports": "error",
       "prettier/prettier": ["error", prettierOptions],
       "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/prefer-nullish-coalescing": "off",
@@ -44,6 +48,26 @@ export const baseConfig = tseslint.config(
           allow: ["location", "event", "history", "name", "status", "Option", "test", "expect", "jest"],
         },
       ],
+      "unused-imports/no-unused-imports": "error",
+      "import/order": [
+        "error",
+        {
+          "groups": ["external", "builtin", "internal", "type", "parent", "sibling", "index", "object"],
+          "alphabetize": {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "warnOnUnassignedImports": true,
+          "newlines-between": "always",
+        },
+      ],
+      "complexity": ["error", { max: 10 }],
+      "max-lines-per-function": ["warn", { max: 50, skipBlankLines: true, skipComments: true }],
+      "max-params": ["error", { max: 3 }],
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "no-unreachable": "error",
+      "prefer-const": "error",
     },
   },
   {
