e1e0eae1f3b9144072e8be90dae0d19b3b1229c8
Nico Serrano <nnicosp@hotmail.com>
2026-07-27 17:58:32 -0500

    fix(repo): ignore dependabot commits

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,6 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  ignores: [(message) => /Signed-off-by: dependabot\[bot]/m.test(message)],
   rules: {
     "scope-empty": [2, "never"],
     "subject-empty": [2, "never"],
